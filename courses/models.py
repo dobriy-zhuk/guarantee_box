9ea8add4d8a142d43cf143fb6c9cd8a4d00a444a
IanKirsh <kirsh@garantylearning.com>
2020-05-31 19:33:43 +0300

    del unnecessary func

--- courses/models.py
+++ courses/models.py
@@ -72,23 +72,6 @@ class Course(models.Model):
     def __str__(self):
         return self.title
 
-    # def get_user_course_completion_rate(self, user) -> str:
-    #     """Returns course completion rate by user.
-
-    #     Arguments:
-    #         user {[type]} -- [description]
-
-    #     Returns:
-    #         [type] -- [description]
-    #     """
-    #     for module in self.modules.all():
-    #         if user.has_perm('done_module', module):
-    #             counter += 1
-        
-    #     percent = (100 * (counter)) / self.modules.count()
-        
-    #     return '{0}% {1}'.format(percent, counter)            
-
 
 class Module(models.Model):
     """Describe courses_module table in db.
